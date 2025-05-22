c418c4a6e80262cb2b10ca52bc5479e46a806b6e
Aditya <bit2swaz@gmail.com>
2025-05-22 18:36:00 +0000

    fixed console error, added updateDisplay function

--- script.js
+++ script.js
@@ -5,22 +5,20 @@ let waitingForSecondOperand = false;
 
 const mainDisplay = document.querySelector('.main-display');
 const historyDisplay = document.querySelector('.history-display');
-const buttons = document.querySelector('.button');
+const buttons = document.querySelectorAll('.button');
 
-mainDisplay.textContent = currentDisplayValue;
+function updateDisplay() {
+    mainDisplay.textContent = currentDisplayValue;
+}
+
+updateDisplay();
 historyDisplay.textContent = '';
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(`Button clicked: ${button.textContent}`);
-    });
-});
-
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-const divide = (a,b) => {
-    if (b === 0) return 'ERROR: Div/0!';
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b === 0) return "ERROR: Div/0!";
     return a / b;
 };
 
@@ -28,7 +26,7 @@ const operate = (op, num1, num2) => {
     num1 = Number(num1);
     num2 = Number(num2);
 
-    switch(op) {
+    switch (op) {
         case '+': return add(num1, num2);
         case '-': return subtract(num1, num2);
         case '*': return multiply(num1, num2);
